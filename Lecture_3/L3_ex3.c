b5e5ed9c5d461d529b3d507122cf55005092b274
Oleh Seheda <sehedao@yahoo.com>
2018-09-25 01:11:04 +0300

    Task 3 done. Add c-file and header file with functions.

---
+++ Lecture_3/L3_ex3.c
@@ -0,0 +1,63 @@
+/******************************************************************************
+
+Create linked list library with functions (ListAdd(), ListIs(), ListRemove()).
+New elements in list should be dynamically allocated.
+
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include "ListLib.h"
+
+
+
+int main()
+{
+    link_list_t *list_init = NULL;
+    link_list_t *for_debug = NULL;
+    int index_in_list;
+    
+    list_init = malloc(sizeof(link_list_t));
+    
+    if (list_init == NULL)
+    {
+        printf("Malloc error\n");
+        return -1;
+    }
+    list_init -> number = 1;
+    list_init -> digit = 0.5;
+    list_init -> new_element = NULL;
+    
+    for (int i = 0; i < 10; i++)
+    {
+        ListAdd (list_init, rand()%10, rand()%10/1.0);
+        
+    }
+    printf("\n");
+    
+    /* * * * * * debug * * * * * * * * * * * * * * * * * *
+    for_debug = list_init;
+    while (for_debug->new_element != NULL)
+    {
+        for_debug = for_debug->new_element;
+    }
+
+    printf("Actual int value in the list is: %d\n",for_debug ->number);
+     * * * * * * * * * * * * * * * * * * * * * * * * * */
+    ListShows(list_init);
+    
+    index_in_list = ListIs (list_init, 2, 1.5);
+    if (index_in_list == -1)
+    {
+        printf("Didn't found item in the list\n");
+    }
+    else
+    {
+        printf("Item found. Item has index %d\n", index_in_list);
+    }
+    
+    ListRemove (list_init);
+    
+    return 0;
+    
+}
---
+++ Lecture_3/ListLib.h
@@ -0,0 +1,124 @@
+/***************************************
+
+Linked list library with functions (ListAdd(), ListIs(), ListRemove()). 
+New elements in list is  dynamically allocate.
+
+***************************************/
+
+typedef struct link_list
+{
+    int number;
+    float digit;
+    struct link_list *new_element;
+    
+} link_list_t;
+
+/**************************************
+
+function ListAdd: adding new item in the end of the list
+
+**************************************/
+void ListAdd (link_list_t *ptr_list, int input_num, float input_dig)
+{
+    
+    link_list_t * current_list = ptr_list;
+    
+     // go to the end of the current list
+    while (current_list -> new_element != NULL)
+    {
+        current_list = current_list -> new_element;
+    }
+
+    // in the end of current list adding new item
+    
+    current_list -> new_element = malloc(sizeof(link_list_t));
+    
+    // item filling
+    current_list -> new_element -> number = input_num;
+    current_list -> new_element -> digit = input_dig;
+    current_list -> new_element -> new_element = NULL;
+}
+/**************************************
+
+function ListIs: finding item in the list
+
+If item is in the list, function returns his index in the list. 
+If not function returns -1.
+
+**************************************/
+int ListIs (link_list_t *ptr_list, int input_num, float input_dig)
+{
+    
+    link_list_t * current_list = ptr_list;
+    int index = 0;
+     // go step by step to the end of the current list
+    while (current_list -> new_element != NULL)
+    {
+        if (current_list -> number == input_num && 
+            current_list -> digit == input_dig)
+        {
+            return index;
+        }
+        else
+        {
+            current_list = current_list -> new_element;
+            index ++;
+        }
+    }
+
+    return -1;
+}
+/**************************************
+
+function ListShows: shows all items in the list
+
+***************************************/
+void ListShows (link_list_t *ptr_list)
+{
+    
+    link_list_t * current_list = ptr_list;
+    
+     // go step by step and printing integer current item
+     printf("\nInteger value in the list:\n");
+    while (current_list -> new_element != NULL)
+    {
+        printf(" %d",current_list -> number);
+        current_list = current_list -> new_element;
+    }
+    
+    current_list = ptr_list;
+    // go step by step and printing floating current item
+    printf("\nFloat value in the list:\n");
+        while (current_list -> new_element != NULL)
+    {
+        printf(" %.2f",current_list -> digit);
+        current_list = current_list -> new_element;
+    }
+    printf("\n");
+}
+/**************************************
+
+function ListRemove: remove last items in the list
+
+***************************************/
+void ListRemove (link_list_t *ptr_list)
+{
+    link_list_t * current_list = ptr_list;
+    int value_n;
+    float value_d;
+    
+     // go to the end of the current list
+    while (current_list -> new_element != NULL)
+    {
+        current_list = current_list -> new_element;
+    }
+
+    // in the end of current list removing item
+    value_d = current_list -> digit;
+    value_n = current_list -> number;
+    
+    free(current_list -> new_element);
+    
+    current_list -> new_element = NULL;
+}
+
