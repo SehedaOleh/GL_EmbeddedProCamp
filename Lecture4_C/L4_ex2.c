5c80b8ec1791643893004336bc8129ad3da8b196
Oleh Seheda <sehedao@yahoo.com>
2018-09-25 23:56:49 +0300

    Start of second task.

---
+++ Lecture4_C/L4_ex2.c
@@ -0,0 +1,15 @@
+/******************************************************************************
+
+Create stack library based on linked list 
+
+*******************************************************************************/
+#include <stdio.h>
+#include "Stack_lib.h"
+
+int main()
+{
+   // Stack initialization
+    stack_t *init_stack;
+    
+   return 0;
+}
