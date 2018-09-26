ee35eba052008079a3718e64d2d4e8e7a181a261
Oleh Seheda <sehedao@yahoo.com>
2018-09-26 23:55:45 +0300

    Second task done. Created stack library based on linked list with function init, push, pop, read.

--- Lecture4_C/L4_ex2.c
+++ Lecture4_C/L4_ex2.c
@@ -9,7 +9,18 @@ Create stack library based on linked list
 int main()
 {
    // Stack initialization
-    stack_t *init_stack;
+    stack_init();
     
-   return 0;
+    stack_push(1);
+    stack_push(2);
+    stack_push(4);
+    stack_push(7);
+    
+    printf("reading from stack %d\n", stack_read ());
+    stack_pop();
+    
+    printf("reading from stack %d\n", stack_read ());
+    
+    printf("Pop from stack %d\n", stack_pop());
+    return 0;
 }
--- Lecture4_C/Stack_lib.h
+++ Lecture4_C/Stack_lib.h
@@ -4,15 +4,70 @@ Stack library based on linked list
 
 *******************************************************************************/
 # include <stdint.h>
+# include <stdlib.h>
 
-typedef struct Stack
+struct Stack
 {
     uint8_t data;
     struct Stack *new_element;
-}stack_t;
+};
+
+typedef struct Stack stack_t; //
+
+stack_t *stack_pointer; // creation of the init stack pointer.
+
+/******************************************************************************
+ 
+Stack init function
+
+******************************************************************************/
+
+void stack_init()
+{
+    stack_pointer = NULL; // initialize the stack by making the init stack pointer = NULL
+}
 
 /******************************************************************************
  
 Stack push function
 
 ******************************************************************************/
+
+void stack_push (uint8_t number)
+{
+    stack_t *tmp;
+    tmp = malloc(sizeof(stack_t));
+    tmp -> data = number; //cteate new element
+    
+    tmp -> new_element = stack_pointer; // pointer to previous position
+    
+    stack_pointer = tmp; // pointer to top of the stack
+   
+}
+
+/******************************************************************************
+ 
+Stack pop function
+
+******************************************************************************/
+
+uint8_t stack_pop ()
+{
+    stack_t *tmp;
+    uint8_t data_temp;
+    
+    tmp = stack_pointer;
+    data_temp = tmp -> data;
+    stack_pointer = stack_pointer -> new_element;
+    free(tmp);
+    return data_temp;  
+}
+/******************************************************************************
+ 
+Stack read function
+
+******************************************************************************/
+uint8_t stack_read ()
+{
+     return stack_pointer -> data;
+}
