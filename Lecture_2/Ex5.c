0359e6a917acc60660a7f5d44aa46e4c1223e058
Oleh Seheda <sehedao@yahoo.com>
2018-09-30 20:29:17 +0300

    Ex5 changes done. Added function for factorial.

--- Lecture_2/Ex5.c
+++ Lecture_2/Ex5.c
@@ -6,17 +6,24 @@
 
 #include <stdio.h>
 #include <stdint.h>
-
+uint64_t factorial(uint8_t base);
 int main()
 {
     uint8_t fact_base_number = 10;
-    uint32_t result = 1;
-   
-    for (int i = 1; i <= fact_base_number; i++)
-    {
-       result*=i;
-    }
+    uint64_t result = 0;
+    
+    result = factorial (fact_base_number);
+    
     
     printf("Factorial of %d is %d", fact_base_number, result);
     return 0;
+}
+uint64_t factorial(uint8_t base)
+{
+    uint64_t result = 1;
+    for (int i = 1; i <= base; i++)
+    {
+        result *= i;
+    }
+    return result;
 }
