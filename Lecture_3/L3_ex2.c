3c895103a21548756c314f73c278973c442c045e
Oleh Seheda <sehedao@yahoo.com>
2018-10-04 00:23:49 +0300

    Added changes according Alexandr comments. Added lib stdlib.h. Change define to upper case. Removed : "max_number = digit_array[0];"

--- Lecture_3/L3_ex2.c
+++ Lecture_3/L3_ex2.c
@@ -8,34 +8,33 @@ Create function to find the biggest element in integer array
 #include <string.h>
 #include <stdint.h>
 #include <time.h>
+#include <stdlib.h>
 
-#define array_length 25
+#define ARRAY_LENGTH 25
 
 int max_element (int *array_input, int length);
 int main()
 {
-    //const uint8_t array_length = 25;
-    int digit_array[array_length] = {};
+    
+    int digit_array[ARRAY_LENGTH] = { 0 };
     int max_number;
     
     srand(time(NULL));
    
-    for(int i = 0; i < array_length; i++)
+    for(int i = 0; i < ARRAY_LENGTH; i++)
     {
         digit_array[i] = rand() % 90 + 10;
         printf("%d ", digit_array[i]);
     } 
-    
-    max_number = digit_array[0];
-    
-    max_number = max_element(digit_array, array_length);
-    printf("\n Max number in array is %d",max_number);
+    max_number = max_element(digit_array, ARRAY_LENGTH);
+    printf("\n Max number in array is %d\n",max_number);
     return 0;
 }
 int max_element (int *array_input, int length)
 {
     int max;
     max = array_input[0];
+    
     for (int i = 0; i < length; i++)
     {
         if (max < array_input[i])
