8011c83bc626d09a9f01e76248ad3cd4fe6db09f
Oleh Seheda <sehedao@yahoo.com>
2018-10-15 23:03:20 +0300

    Ex2 functions in library modified according to Alexanders inputs.

--- Lecture4_C/L4_ex2.c
+++ Lecture4_C/L4_ex2.c
@@ -9,18 +9,18 @@ Create stack library based on linked list
 int main()
 {
    // Stack initialization
-    stack_init();
+    stack_t *stack_p1 = NULL; // creation of the init stack pointer.
     
-    stack_push(1);
-    stack_push(2);
-    stack_push(4);
-    stack_push(7);
+    stack_push(&stack_p1, 1);
+    stack_push(&stack_p1, 2);
+    stack_push(&stack_p1, 4);
+    stack_push(&stack_p1, 7);
     
-    printf("reading from stack %d\n", stack_read ());
-    stack_pop();
+    printf("reading from stack %d\n", stack_read (&stack_p1));
+    stack_pop(&stack_p1);
     
-    printf("reading from stack %d\n", stack_read ());
+    printf("reading from stack %d\n", stack_read (&stack_p1));
     
-    printf("Pop from stack %d\n", stack_pop());
+    printf("Pop from stack %d\n", stack_pop(&stack_p1));
     return 0;
 }
