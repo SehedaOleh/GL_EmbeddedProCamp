1356d58a43371463ab42e81fe98365426432ef87
Oleh Seheda <sehedao@yahoo.com>
2018-10-28 10:23:09 +0200

    Add new task manually.

--- Lecture9/Src/main.c
+++ Lecture9/Src/main.c
@@ -15,6 +15,8 @@ osThreadId myTask03Handle;
 void StartDefaultTask(void const * argument);
 void StartTask02(void const * argument);
 void StartTask03(void const * argument);
+void vmyTaskCode( void* pvParameters);
+void vOtherFunction( void );
 
 void vApplicationIdleHook(void)
 {
@@ -49,8 +51,13 @@ int main(void)
   /* definition and creation of myTask03 */
   osThreadDef(myTask03, StartTask03, osPriorityIdle, 0, 128);
   myTask03Handle = osThreadCreate(osThread(myTask03), NULL);
-
-  /* Start scheduler */
+	
+	vOtherFunction();
+	
+	
+	
+	/* definition and creation of Task */
+	/* Start scheduler */
   osKernelStart();
 
   while (1)
@@ -58,6 +65,41 @@ int main(void)
   }
 }
 
+/* Task to be created. */
+	void vmyTaskCode( void* pvParameters)
+	{
+			/* The parameter value is expected to be 1 as 1 is passed in the
+			pvParameters value in the call to xTaskCreate() below. 
+			configASSERT( ( ( uint32_t ) pvParameters ) == 1 );*/
+
+			for( ;; )
+			{
+					/* Task code goes here. */
+				osDelay(1);
+			}
+	}
+	/* Function that creates a task. */
+	
+	void vOtherFunction(void)
+	{
+	BaseType_t xReturned;
+	TaskHandle_t xHandle = NULL;
+
+			/* Create the task, storing the handle. */
+			xReturned = xTaskCreate(
+											vmyTaskCode,       /* Function that implements the task. */
+											"NAME",          /* Text name for the task. */
+											16,      /* Stack size in words, not bytes. */
+											( void * ) 1,    /* Parameter passed into the task. */
+											tskIDLE_PRIORITY,/* Priority at which the task is created. */
+											&xHandle );      /* Used to pass out the created task's handle. */
+
+			if( xReturned == pdPASS )
+			{
+					/* The task was created.  Use the task's handle to delete the task. */
+					//vTaskDelete( xHandle );
+			}
+		}
 /* StartDefaultTask */
 void StartDefaultTask(void const * argument)
 {
