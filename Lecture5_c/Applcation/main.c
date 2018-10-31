92f227fcb97364329dd180eadc621d5f8dfd3195
Oleh Seheda <sehedao@yahoo.com>
2018-10-31 23:52:08 +0200

    Added delay_ms function according to Alexander recommendation.

--- Lecture5_c/Applcation/main.c
+++ Lecture5_c/Applcation/main.c
@@ -3,6 +3,7 @@
 #include "USART.h"
 
 # define RxLength 32
+#define TIM_PERIOD (8000)
 
 static volatile uint32_t runtime = 0;
 	
@@ -12,12 +13,18 @@ static volatile uint32_t runtime = 0;
 
 void RestoreConfiguration(void);
 void delay_ms(uint32_t);
+void timer_init(void);
+void clk_init(void);
 
 int main(void)
 {
 	//string for debug
 	 static uint8_t UART_rxdata[10] = {"adsr"};
 	//
+	 
+	clk_init();
+  timer_init();
+	 
 	UART1_init();
 	UART1_open();
   while(1)
@@ -85,3 +92,64 @@ void delay_ms(uint32_t delay)
       /* Wait while runtime variable reach target_runtime value */
     }
 }
+/**
+	* @brief  This is an interrupt handler routine for TIM4.
+  *         It just check if source of interrupt is Timer Update event.
+  *         If yes, then it updates runtime counter and clear pending bit.
+  *
+  * @retval None
+  */
+void TIM4_IRQHandler(void)
+{
+  if(TIM_GetITStatus(TIM4, TIM_IT_Update) == SET)
+    {
+      runtime++;
+      TIM_ClearITPendingBit(TIM4, TIM_IT_Update);
+    }
+}
+void timer_init(void)
+{
+  TIM_TimeBaseInitTypeDef tim = {0};
+  NVIC_InitTypeDef nvic = {0};
+  
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+  
+  /* Period 1ms */
+  tim.TIM_Prescaler = 0;
+  tim.TIM_CounterMode = TIM_CounterMode_Up;
+  tim.TIM_Period = TIM_PERIOD;
+  tim.TIM_ClockDivision = TIM_CKD_DIV1;
+  tim.TIM_RepetitionCounter = 0;
+
+  TIM_TimeBaseInit(TIM4, &tim);
+
+  /* Enable the TIM4 global Interrupt */
+  nvic.NVIC_IRQChannel = TIM4_IRQn;
+  nvic.NVIC_IRQChannelPreemptionPriority = 0;
+  nvic.NVIC_IRQChannelSubPriority = 1;
+  nvic.NVIC_IRQChannelCmd = ENABLE;
+
+  NVIC_Init(&nvic);
+  
+  TIM_ITConfig(TIM4, TIM_IT_Update, ENABLE);
+
+  TIM_Cmd(TIM4, ENABLE);
+}
+
+/**
+  * @brief This function set up system clock source as High Speed Internal (HSI) source
+  * 
+  * @retval
+  */
+void clk_init(void)
+{
+  RCC_HSICmd(ENABLE);
+
+  while(RCC_GetFlagStatus(RCC_FLAG_HSIRDY) != SET)    
+    {
+      /* Wait while HSI source is not ready */
+    }
+    
+  RCC_SYSCLKConfig(RCC_SYSCLKSource_HSI);
+}
+
